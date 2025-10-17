e5c45963eaa075544b4d683be4b93bf8fe5a932b
Afia Rahman <rahmanmou26@gmail.com>
2025-10-17 22:56:02 +0600

    Third commit

--- practice.js
+++ practice.js
@@ -2,7 +2,7 @@ console.log*("hello world")
 
 console.log("New practice session")
 
-let x = parseInt(prompt("Enter a number"))
-let y = parseInt(prompt("Enter another number"))
-let z =x+y
-console.log(z)
+// let x = parseInt(prompt("Enter a number"))
+// let y = parseInt(prompt("Enter another number"))
+// let z =x+y
+// console.log(z)
