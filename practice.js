dbb94120550c4a1289ed9f57d68a6491f8466dfa
Afia Rahman <rahmanmou26@gmail.com>
2025-10-20 12:29:35 +0600

    arithmetic part added

--- practice.js
+++ practice.js
@@ -6,3 +6,10 @@ console.log("New practice session")
 // let y = parseInt(prompt("Enter another number"))
 // let z =x+y
 // console.log(z)
+
+let a = 3, b=5, c=9;
+let x = a + b + c;
+let y = c - a;
+let z = (b + c) * a;
+let i = c/b
+console.log(x, y, z, i)
