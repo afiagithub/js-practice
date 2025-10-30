cffc7ac55066c7dafc6425d47eb2b30fb29d3afa
Afia Rahman <rahmanmou26@gmail.com>
2025-10-30 17:15:03 +0600

    conditional statement added

--- practice.js
+++ practice.js
@@ -13,3 +13,31 @@ let y = c - a;
 let z = (b + c) * a;
 let i = c/b
 console.log(x, y, z, i)
+
+let mark=31
+if(mark<0 || mark>100){
+    console.log("Invalid input")
+}
+else{
+    if(mark>80){
+        console.log("A+")
+    }
+    else if(mark>70 && mark<80){
+        console.log("A")
+    }
+    else if(mark>70 && mark<80){
+        console.log("A-")
+    }
+    else if(mark>60 && mark<70){
+        console.log("B")
+    }
+    else if(mark>33 && mark<60){
+        console.log("C")
+    }
+    else if(mark<33){
+        console.log("F")
+    }
+    else{
+        console.log("Invalid input")
+    }
+}
